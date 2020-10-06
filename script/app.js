6b04e1e0332c15f8b4853ee620cb40655abc68c8
debeerst louis <55881630+DebeerstLouis@users.noreply.github.com>
2020-10-06 12:15:36 +0200

    mid

--- script/app.js
+++ script/app.js
@@ -3,7 +3,12 @@ function handleFloatingLabel() {
 }
 
 function handlePasswordSwitcher() {
-    
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
